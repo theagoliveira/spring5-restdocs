b4a526064083daeea7df34306dfd47c38ce9264a
Thiago Cavalcante <thiago.kun@gmail.com>
2021-08-29 03:18:39 -0300

    Prepare app for deployment on Heroku
    According to a post on Heroku Dev Center.
    
    Changes:
    - Force the use of HTTPS with the WebSecurityConfig
    - Disable CSRF in the config as well
    - Rate-limit API calls with Bucket4j (and Ehcache)
    - Specify a Java version for Heroku using system.properties file
    - Add Heroku Maven Plugin to pom
    - Update Postman collection JSON file

---
+++ src/main/java/guru/springframework/sfgrestdocsexample/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgrestdocsexample.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableCaching
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel()
+            .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+            .requiresSecure()
+            .and()
+            .csrf()
+            .disable();
+    }
+
+}
